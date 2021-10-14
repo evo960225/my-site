d4dbedb7e4290d8a6e27659348f06ffa73ee9fe2
unknown <evo960225@gmail.com>
2021-10-14 21:15:29 +0800

    3D page canvas v-for

--- src/components/D3.vue
+++ src/components/D3.vue
@@ -17,9 +17,15 @@
       Model in Three.js
     </h2>
     <div class="flex justify-evenly flex-wrap">
-      <div id="donut" class="mx-2 my-4"></div>
-      <div id="cube" class="mx-2 my-4"></div>
-      <div id="cup" class="mx-2 my-4"></div>
+      <div v-for="name in models_name" :id="name" :key="name"
+           class="mx-2 my-4">
+        <div class="loading-3d-anime flex flex-wrap">
+          <img class="mx-auto mt-auto mb-3" src="../assets/hoshi-loading.png" />
+          <div class="w-full text-white text-2xl tracking-030 opacity-30"> 
+            Loading...
+          </div>
+        </div>
+      </div>
     </div>
  </div>
 </template>
@@ -31,10 +37,16 @@
   import init_scene from '../ts/init_scene';
 
   export default defineComponent({
+    data(){
+      return{
+        models_name: ['donut','cube','cup']
+      }
+    },
     async mounted() {   
       const canvas = document.querySelector('#donut');
       if(canvas !== null){
         const model = await load_model('donut');
+        canvas.innerHTML = '';
         model.position.set( 0, 0, 0 );
         model.scale.set( 5, 5, 5 );
         init_scene(canvas, model);
@@ -42,6 +54,7 @@
       const canvas2 = document.querySelector('#cube');
       if(canvas2!==null){
         const model = await load_model('cube');
+        canvas2.innerHTML = '';
         model.position.set( 0, 0, 0 );
         model.scale.set( 0.2, 0.2, 0.2 );
         init_scene(canvas2, model);
@@ -49,6 +62,7 @@
       const canvas3 = document.querySelector('#cup');
       if(canvas3!==null){
         const model = await load_model('cup');
+        canvas3.innerHTML = '';
         model.position.set( 0, 0, 0 );
         model.scale.set( 5.5, 5.5, 5.5 );
         init_scene(canvas3, model);
@@ -60,3 +74,62 @@
  
 
 </script>
+<style>
+.loading-3d-anime {
+    width: 600px;
+    height: 400px;
+    background-color: rgb(63, 62, 68);
+    transition: all 0.7s ease;
+    opacity: 1;
+    visibility: visible;
+    background-attachment: fixed;
+    background-image: url('/src/assets/bg2.svg');
+}
+.loading-3d-anime > img{
+    width: 100px;
+    height: 100px;
+    opacity: 0.3;
+    animation: loader-3d 2.6s infinite;
+}
+@keyframes loader-3d {
+  0%{
+    transform: translateY(-10px) rotateY(0deg);
+  }
+  6%{
+    transform: translateY(0px) rotateY(0deg);
+  }
+  12%{
+    transform: translateY(-10px) rotateY(0deg);
+  }
+  18% {
+    transform: translateY(0px) rotateY(0deg);
+  }
+  24%{
+    transform: translateY(-10px) rotateY(0deg);
+  }
+  35%{
+    transform: translateY(-10px) rotateY(180deg);
+  }
+  50%{
+    transform: translateY(-10px) rotateY(180deg);
+  }
+  56%{
+    transform: translateY(0px) rotateY(180deg);
+  }
+  62%{
+    transform: translateY(-10px) rotateY(180deg);
+  }
+  68%{
+    transform: translateY(0px) rotateY(180deg);
+  }
+  74% {
+    transform: translateY(-10px) rotateY(180deg);
+  }
+  85%{
+    transform: translateY(-10px) rotateY(0deg);
+  }
+  100%{
+    transform: translateY(-10px) rotateY(0deg);
+  }
+}
+</style>
--- src/ts/load_glb_model.ts
+++ src/ts/load_glb_model.ts
@@ -2,6 +2,7 @@
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+
 const loader = new GLTFLoader();
 function modelLoader(url: string) {
     return new Promise<Group>((resolve, reject) => {
@@ -10,7 +11,9 @@ function modelLoader(url: string) {
 }
 
 export default async function load_model(model_name: string) {
-    const modelUrl = new URL('../assets/3D_model/' + model_name + '.glb', import.meta.url);
-    const model = await modelLoader(modelUrl.href);
+    const modelUrl = './3D_model/' + model_name + '.glb';
+    const model = await modelLoader(modelUrl);
+    // local animate test
+    await new Promise((resolve)=>setTimeout(()=>{ resolve('load ok'); }, 5000));
     return model;
 }
